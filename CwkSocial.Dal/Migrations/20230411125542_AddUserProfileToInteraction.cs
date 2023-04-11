d27c9306f260fef02bd77faebe933513af9312d9
Gëzim Shabani <gezim.shabani@ikubinfo.al>
2023-04-11 14:56:55 +0200

    migration

---
+++ CwkSocial.Dal/Migrations/20230411125542_AddUserProfileToInteraction.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CwkSocial.Dal.Migrations
+{
+    public partial class AddUserProfileToInteraction : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
